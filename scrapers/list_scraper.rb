62bca345b43537424a96c028315a681ca908d274
polyglot-chameleon <oleg.harlamov@web.de>
2024-09-13 02:01:50 +0200

    refactor (list) scrapers, WIP #5

--- scrapers/list_scraper.rb
+++ scrapers/list_scraper.rb
@@ -7,16 +7,18 @@ require_relative 'scraper'
 class ListScraper < Scraper
   def initialize(source)
     super source
-    api_url = URI.join ENV['API_URL'], '/latest', "?src_url=#{@url.host}"
-    resp = HTTParty.get api_url
+    resp = HTTParty.get URI.join ENV['API_URL'], '/latest', "?src_url=#{@url.host}"
     @max_datetime = resp.body != 'null' ? DateTime.parse(resp.body) : DateTime.new
-    @cur_page_idx = 1
+
+    @items = []
   end
 
   # Scrape the contents of a page
+  #
+  # overrides super
   def run
-    page_data = collect_items_from fetch_html
-    create page_data
+    collect_items_from fetch_html
+    create
   end
 
   protected
@@ -25,40 +27,32 @@ class ListScraper < Scraper
   #
   # `doc` *Nokogiri::HTML5::Document*
   def collect_items_from(doc)
-    page_data = []
     dom_items = doc.css @src['item_css']
 
     dom_items.each_with_index do |dom_item, idx|
-      html_data = collect_data dom_item
-      html_data = extract_regex html_data if @src['text_css'].key? 'meta'
-      page_data[idx] = html_data
+      collect_data dom_item
+      extract_regex if @src['text_css'].key? 'meta'
+      @items[idx] = @item
     end
 
-    return page_data unless @src.key? 'pagewise'
+    pagewise dom_items if @src.key? 'pagewise'
+  end
 
-    # Fetch data on individual page
-    progressbar = ProgressBar.create title: "Page #{@cur_page_idx}", total: dom_items.length
-
+  # Fetch data on individual page
+  def pagewise(dom_items)
     (0..dom_items.length - 1).each do |idx|
-      page_src = {}
-      page_src['url'] = @url.host + page_data[idx]['url']
+      page_src = { url: @url.host + @items[idx]['url'] }
       page_src.merge!(**@src['pagewise'])
 
       page_scraper = Scraper.new page_src
-      page_scraper.cookie = @cookie
-      page_data[idx].merge! page_scraper.run
-      progressbar.increment
+      @items[idx].merge! page_scraper.run
     end
-
-    page_data
   end
 
-  # Create posts via API
-  #
-  # `posts` *Array*
-  def create(posts)
+  # Create posts
+  def create
     HTTParty.post ENV['API_URL'],
-                  { body: posts.to_json,
+                  { body: @items.to_json,
                     headers: { 'Content-Type' => 'application/json' } }
   end
 end
--- scrapers/scraper.rb
+++ scrapers/scraper.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'nokogiri'
 require 'logger'
 
-# Generic scraper
+# Generic item scraper
 #
 # `src` *Hash*
 class Scraper
@@ -24,8 +24,8 @@ class Scraper
   #
   # `return` *Hash*
   def run
-    dom = fetch_html
-    extract_regex collect_data dom
+    collect_data fetch_html
+    extract_regex
   end
 
   protected
@@ -49,75 +49,83 @@ class Scraper
   # Collect data from dom document
   #
   # `dom_doc`: *Nokogiri::HTML5::Document*
-  #
-  # `return` *Hash*
-  def collect_data(dom_doc) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
-    item = { url: self.class.send(:get_url, dom_doc, @src['link_css'], 'href') || @url }
+  def collect_data(dom_doc)
+    @item = { url: self.class.send(:get_attr_val, dom_doc, @src['link_css'], 'href') || @url }
 
-    @src['text_css'].each_key do |key|
-      html_data = dom_doc.at_css(@src['text_css'][key])
-      next if html_data.nil?
+    (@src.key? 'text_css') && extract_text_sections(dom_doc)
+    (@src.key? 'multivalue') && extract_multivalue(dom_doc)
+    (@src.key? 'by_attr') && extract_attr_vals(dom_doc)
 
-      item[key] = html_data.content.strip
-    end
-
-    (@src.key? 'multivalue') && @src['multivalue'].each_key do |key|
-      html_data_list = dom_doc.css(@src['multivalue'][key])
-
-      item[key] = html_data_list.map { |html_item| html_item.content.strip }
-    end
-
-    (@src.key? 'by_attr') && @src['by_attr'].each do |by_attr|
-      by_attr['attrs'].each do |attr|
-        name = by_attr.key?('name') ? "#{by_attr['name']}_#{attr}" : attr
-        attr_value = self.class.send(:get_url, dom_doc, by_attr['css'], attr)
-        item[name] = attr_value unless attr_value.nil?
-      end
-    end
-
-    item.transform_keys(&:to_s)
+    @item.transform_keys(&:to_s)
   end
 
   # Extract regex from item
-  #
-  # `page` *Hash*
-  def extract_regex(page) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+  def extract_regex
     (@src.key? 'regex') && @src['regex'].each_key do |src_var|
-      src_var_split, idx = src_var.split('__')
-      next unless page.key? src_var_split
-
-      @src['regex'][src_var].each_key do |key|
-        matches = (if idx
-                     page[src_var_split][idx.to_i]
-                   else
-                     page[src_var]
-                   end).gsub(/\n|\s{2,}|\u00A0/,
-                             ' ').match(@src['regex'][src_var][key]).to_a
-        page[key] = matches[matches.length > 1 ? 1 : 0]
-      rescue NoMethodError => e
-        @log.warn e.message
-      end
+      get_regex_at src_var.split('__')
     end
 
-    return page unless page.key?('datetime') && !page['datetime'].nil?
-
-    page['datetime'] =
-      if page.key? 'datetime_fmt'
-        DateTime.strptime page['datetime'],
-                          @src['datetime_fmt']
-      else
-        DateTime.parse page['datetime']
-      end
-
-    page
+    extract_datetime
   end
 
   private
 
-  def self.get_url(dom, css, attr_name)
-    node = dom.at_css(css)
+  # Extract text sections
+  def extract_text_sections(dom_doc)
+    @src['text_css'].each_key do |section|
+      html_section = dom_doc.at_css @src['text_css'][section]
+      next if html_section.nil?
+
+      @item[section] = html_section.content.strip
+    end
+  end
+
+  # Extract multivalue entities
+  def extract_multivalue(dom_doc)
+    @src['multivalue'].each_key do |key|
+      html_data_list = dom_doc.css(@src['multivalue'][key])
+
+      @item[key] = html_data_list.map { |html_item| html_item.content.strip }
+    end
+  end
+
+  # Extract attribute values
+  def extract_attr_vals(dom_doc)
+    @src['by_attr'].each do |by_attr|
+      by_attr['attrs'].each do |attr|
+        name = by_attr.key?('name') ? "#{by_attr['name']}_#{attr}" : attr
+        attr_val = self.class.send(:get_attr_val, dom_doc, by_attr['css'], attr)
+        @item[name] = attr_val unless attr_val.nil?
+      end
+    end
+  end
+
+  # Regex match on previously extracted html entity
+  def get_regex_at(src_var, idx) # rubocop:disable Metrics/AbcSize
+    @src['regex'][src_var].each_key do |key|
+      matches = (if idx
+                   @item[src_var][idx.to_i] # if multivalue entity
+                 else
+                   @item[src_var]
+                 end).gsub(/\n|\s{2,}|\u00A0/,
+                           ' ').match(@src['regex'][src_var][key]).to_a
+
+      @item[key] = matches[matches.length % 2] # > 1 ? 1 : 0
+    end
+  end
+
+  # Parse previously extracted datetime
+  def extract_datetime
+    (@src.key? 'datetime_fmt') && (return DateTime.strptime @item['datetime'],
+                                                            @src['datetime_fmt'])
+
+    @item['datetime'] = DateTime.parse @item['datetime']
+  end
+
+  def self.get_attr_val(dom, css, attr_name)
+    node = dom.at_css css
     node && node[attr_name]
   end
 
-  private_class_method :get_url
+  private_class_method :get_attr_val
 end
